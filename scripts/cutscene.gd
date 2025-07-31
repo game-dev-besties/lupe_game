ec470beffad982a352cb016234ef44abb7a5e8d8
BetaBear2 <90876300+BetaBear2@users.noreply.github.com>
2025-07-31 15:58:10 -0700

    added cutscene transitions

--- scripts/cutscene.gd
+++ scripts/cutscene.gd
@@ -3,11 +3,23 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$Transition/AnimationPlayer.play("fade_in")
 	$AnimationPlayer.play("Cutscene1")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	pass
+	if Input.is_action_pressed("ui_select"):
+		end_cutscene()
+	
+func end_cutscene():
+	$Transition/AnimationPlayer.play("fade_out")
+	$AnimationPlayer.play("stop")
+	await get_tree().create_timer(0.7).timeout
+	get_tree().change_scene_to_packed(game)
 
 func _on_skip_pressed():
-	get_tree().change_scene_to_packed(game)
+	end_cutscene()
+
+func _on_animation_player_animation_finished(anim_name):
+	if (anim_name == "Cutscene1"):
+		end_cutscene()
