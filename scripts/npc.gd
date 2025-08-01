5676944873e99a35181a9dcdcabac846a1db811b
benman604 <57852213+benman604@users.noreply.github.com>
2025-07-31 21:52:38 -0700

    hunger bar

--- scripts/npc.gd
+++ scripts/npc.gd
@@ -10,6 +10,8 @@ var satiation: int = 0
 var desired_item_name: String
 var placement_angle: float 
 var consumption_timer: float
+var hunger_timer: Timer
+var hunger: int = 100
 
 @onready var sprite: Sprite2D = $Sprite2D
 
@@ -18,14 +20,28 @@ func init(data: Dictionary):
 	self.desired_item_name = data.get("desire", "")
 	self.consumption_timer = data.get("timer", 1.0)
 	self.satiation = data.get("satiation", 0)
+	$HealthBar.value = hunger	
 
 func _ready():
 	update_emotion()
+	
+	hunger_timer = Timer.new()
+	hunger_timer.wait_time = 1
+	hunger_timer.one_shot = false
+	hunger_timer.autostart = true
+	add_child(hunger_timer)
+	hunger_timer.connect("timeout", _on_hunger_timeout)
+	
+func _on_hunger_timeout():
+	hunger = max(hunger - 3, 0)
+	$HealthBar.value = hunger
 
 func eat(satiation_value: int):
 	if satiation < MAX_SATIATION:
 		satiation = min(satiation + satiation_value, MAX_SATIATION)
 		print(name + "'s satiation is now: " + str(satiation))
+		hunger = 100
+		$HealthBar.value = hunger
 		update_emotion()
 
 func react(is_correct_dish: bool):
