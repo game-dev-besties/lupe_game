8457a7266939dc963952874fec2ccdda40fbd37a
sohanagarkar <sohanagarkar@gmail.com>
2025-08-02 10:56:43 -0700

    Bug Fix: Health Bar Not Full At Start

--- scripts/game.gd
+++ scripts/game.gd
@@ -13,6 +13,7 @@ var health: int = 100
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Transition/AnimationPlayer.play("fade_in")
+	health_bar.value = health
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
