86fc9dcc79bc3c1b6c5fbafd0c2c1dd26d103294
BetaBear2 <90876300+BetaBear2@users.noreply.github.com>
2025-08-03 01:38:50 -0700

    finalized game loss screen and didn't break it this time

--- scripts/lost_screen.gd
+++ scripts/lost_screen.gd
@@ -7,13 +7,35 @@ func _ready():
 	flute.play()
 	$Transition/AnimationPlayer.play("fade_in")
 
-func _try_again() -> void:
-	$Transition/AnimationPlayer.play("fade_out")
-	await get_tree().create_timer(0.7).timeout
-	get_tree().change_scene_to_packed(game)
-
-
 func menu_button() -> void:
 	$Transition/AnimationPlayer.play("fade_out")
 	await get_tree().create_timer(0.7).timeout
 	get_tree().change_scene_to_packed(game)
+
+
+func _on_try_again_button_mouse_entered() -> void:
+	$UI/TryAgain.scale += Vector2(0.05, 0.05)
+
+func _on_try_again_button_mouse_exited() -> void:
+	$UI/TryAgain.scale -= Vector2(0.05, 0.05)
+
+func _on_try_again_button_gui_input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_accept"):
+		Transition.transition()
+		await Transition.on_transition_finished
+		get_tree().change_scene_to_packed(game)
+
+
+func _on_menu_button_gui_input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_accept"):
+		Transition.transition()
+		await Transition.on_transition_finished
+		get_tree().change_scene_to_packed(menu)
+
+
+func _on_menu_button_mouse_entered() -> void:
+	$UI/Exit.scale += Vector2(0.05, 0.05)
+
+
+func _on_menu_button_mouse_exited() -> void:
+	$UI/Exit.scale -= Vector2(0.05, 0.05)
